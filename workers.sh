0059072bc7cd2a3d3c8537b855cb5dd404648eb4
alexandrosst <alstylos@yahoo.gr>
2023-02-05 13:56:50 +0200

    third commit

--- workers.sh
+++ workers.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-declare -a start_workers=("spark-daemon.sh start org.apache.spark.deploy.worker.Worker 1 --webui-port 8080 --port 65509 --cores 2 --memory 4g spark://192.168.0.2:7077"
-                        "/home/user/spark/sbin/spark-daemon.sh start org.apache.spark.deploy.worker.Worker 3 --webui-port 8080 --port 65511 --cores 2 --memory 4g spark://192.168.0.2:7077")
-declare -a stop_workers=("spark-daemon.sh stop org.apache.spark.deploy.worker.Worker 1 --webui-port 8080 --port 65509 --cores 2 --memory 4g spark://192.168.0.2:7077"
-                        "/home/user/spark/sbin/spark-daemon.sh stop org.apache.spark.deploy.worker.Worker 3 --webui-port 8080 --port 65511 --cores 2 --memory 4g spark://192.168.0.2:7077")
+declare -a start_workers=("spark-daemon.sh start org.apache.spark.deploy.worker.Worker 1 --webui-port 8080 --port 65509 --cores 2 --memory 4g spark://192.168.0.1:7077"
+                        "~/spark-3.1.3-bin-hadoop2.7/sbin/spark-daemon.sh start org.apache.spark.deploy.worker.Worker 2 --webui-port 8080 --port 65510 --cores 2 --memory 4g spark://192.168.0.1:7077")
+declare -a stop_workers=("spark-daemon.sh stop org.apache.spark.deploy.worker.Worker 1 --webui-port 8080 --port 65509 --cores 2 --memory 4g spark://192.168.0.1:7077"
+                        "~/spark-3.1.3-bin-hadoop2.7/sbin/spark-daemon.sh stop org.apache.spark.deploy.worker.Worker 2 --webui-port 8080 --port 65510 --cores 2 --memory 4g spark://192.168.0.1:7077")
 
 if [ $# -lt 2 ]
 then
@@ -24,11 +24,11 @@ then
     if [ $2 = start ]
     then
         eval ${start_workers[0]}
-        ssh user@192.168.0.1 "${start_workers[1]}"
+        ssh user@slave "${start_workers[1]}"
     elif [ $2 = stop ]
     then
         eval ${stop_workers[0]}
-        ssh user@192.168.0.1 "${stop_workers[1]}"
+        ssh user@slave "${stop_workers[1]}"
     else
         echo "You have to use either start or stop"
     fi
